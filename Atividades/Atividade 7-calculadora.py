c8290761e90a62ec77c1937d073444e0518d81ad
Vitor-Mateus-Dev <116602039+Vitor-Mateus-Dev@users.noreply.github.com>
2022-12-01 12:00:17 -0300

    Update Atividade 7-calculadora.py

--- Atividades/Atividade 7-calculadora.py
+++ Atividades/Atividade 7-calculadora.py
@@ -1,13 +1,16 @@
-Rodas = float(input('Rodas'))
-Pesokg = float(input('Peso '))
-Pessoas = float(input ('Pessoas no veiculo'))
-if Rodas<=3 :
-   print ('O veiculo designado é o tipo A ')
-elif Rodas==4 and Pessoas<=8 and Pesokg<=3500:
-    print ('O veiculo designado é o tipo B ')
-elif Rodas>=4 and Pesokg>=3500<=6000:
-    print ('O veiculo designado é o tipo C ')
-elif Rodas>=4 and Pessoas>8:
-    print ('O veiculo designado é o tipo D ')
-elif Rodas>=4 and Pesokg>6000:
-    print ('O veiculo designado é o tipo D ')
+Operação = float(input(
+    "digite se será soma(1), subtração(2), multiplicação(3), divisão(4)"))
+Valor_inicial = int(input("digite um valor"))
+Valor_secundario = int(input("digite o segundo valor"))
+if Operação == (1):
+    resultado_soma = Valor_inicial + Valor_secundario
+    print(resultado_soma)
+elif Operação == (2):
+    resultado_subtração = Valor_inicial - Valor_secundario
+    print(resultado_subtração)
+elif Operação == (3):
+    resultado_multiplicação = Valor_inicial * Valor_secundario
+    print(resultado_multiplicação)
+elif Operação == (4):
+    resultado_divisão = Valor_inicial / Valor_secundario
+    print(resultado_divisão)
